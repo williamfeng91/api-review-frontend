8103f2d9c36001f96c578cdda5b3287f4b473922
William Feng <williamf@webrecs.com.au>
2015-09-19 20:27:22 +1000

    Added dataservice

---
+++ app/shared/services/dataservice.js
@@ -0,0 +1,35 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.services')
+        .factory('dataservice', dataservice);
+
+    /** @ngInject */
+    function dataservice($http) {
+        var service = {
+            getReview: getReview
+        };
+
+        return service;
+        /////////////////////
+
+        function getReview(reviewId) {
+            return $http({
+                url: 'http://jsonstub.com/reviews/' + reviewId,
+                method: 'GET',
+                dataType: 'json', 
+                data: '',         
+                headers: {
+                    'Content-Type': 'application/json',
+                    'JsonStub-User-Key': '51ec5e27-c872-4a8e-8070-dec92f5fca8f',
+                    'JsonStub-Project-Key': 'deeec676-1dad-4086-a1f0-ead970151994'
+                }
+            }).then(getReviewComplete);
+
+            function getReviewComplete(data, status, headers, config) {
+                return data.data;
+            }
+        }
+    }
+}());
