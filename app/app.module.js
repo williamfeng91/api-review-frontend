2f0297281403c6075e226ec9d65ccf389f692318
William Feng <williamf@webrecs.com.au>
2015-09-19 11:06:53 +1000

    Relocated angular lib

--- app/app.module.js
+++ app/app.module.js
@@ -1,12 +1,12 @@
 (function() {
-	'use strict';
+    'use strict';
 
-	angular.module('app', [
-		'ngRoute',
-		'app.services',
-		'app.header',
-		'app.footer',
-		'app.home'
-	]);
+    angular.module('app', [
+        'ngRoute',
+        'app.services',
+        'app.header',
+        'app.footer',
+        'app.home'
+    ]);
 
 })();
