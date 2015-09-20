c4d9b714dc37be3fefb6247462824014b32f92c8
William Feng <williamf@webrecs.com.au>
2015-09-20 12:26:11 +1000

    Added review page

--- app/shared/services/dataservice.js
+++ app/shared/services/dataservice.js
@@ -6,9 +6,10 @@
         .factory('dataservice', dataservice);
 
     /** @ngInject */
-    function dataservice($http) {
+    function dataservice($http, logger) {
         var service = {
-            getReview: getReview
+            getReview: getReview,
+            getReviews: getReviews
         };
 
         return service;
@@ -28,6 +29,24 @@
             }).then(getReviewComplete);
 
             function getReviewComplete(data, status, headers, config) {
+                return data.data;
+            }
+        }
+
+        function getReviews(page, numsPerPage) {
+            return $http({
+                url: 'http://jsonstub.com/reviews?p=' + page + "&n=" + numsPerPage,
+                method: 'GET',
+                dataType: 'json', 
+                data: '',         
+                headers: {
+                    'Content-Type': 'application/json',
+                    'JsonStub-User-Key': '51ec5e27-c872-4a8e-8070-dec92f5fca8f',
+                    'JsonStub-Project-Key': 'deeec676-1dad-4086-a1f0-ead970151994'
+                }
+            }).then(getReviewsComplete);
+
+            function getReviewsComplete(data, status, headers, config) {
                 return data.data;
             }
         }
