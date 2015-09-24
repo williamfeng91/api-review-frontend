00c43593244cf205307d31282166a8f78cb5f9f2
William Feng <williamf@webrecs.com.au>
2015-09-24 18:18:25 +1000

    Updated links

--- app/app.module.js
+++ app/app.module.js
@@ -25,7 +25,7 @@
 
         $rootScope.$on('$locationChangeStart', function (event, next, current) {
             // redirect to login page if not logged in and trying to access a restricted page
-            var restrictedPage = $.inArray($location.path(), ['/login', '/register']) === -1;
+            var restrictedPage = $.inArray($location.path(), ['/login', '/register', '/']) === -1;
             if (restrictedPage && !authservice.isAuthenticated()) {
                 $location.path('/login');
             }
