2f55d71c381101e697c29e6e577b2398c4aa4315
William Feng <williamf@webrecs.com.au>
2015-09-26 17:21:09 +1000

    Updated review editor

--- app/components/review-editor/reviewEditor.js
+++ app/components/review-editor/reviewEditor.js
@@ -5,8 +5,33 @@
         .module('app.reviewEditor')
         .controller('ReviewEditorController', ReviewEditorController);
 
-    function ReviewEditorController($routeParams, dataservice, logger) {
+    /** @ngInject */
+    function ReviewEditorController($location, $routeParams, reviewservice, tagservice,
+        session, logger) {
         var vm = this;
-        logger.log("test");
+
+        vm.review = session.getCurrentReview();
+        vm.loadTags = loadTags;
+        vm.submit = submitReview;
+        vm.cancel = cancelEdit;
+
+        function loadTags(query) {
+            return tagservice.getAll()
+                .then(function (result) {
+                    var tags = result.tags;
+                    return tags.filter(function (tag) {
+                        return tag.name.toLowerCase().indexOf(query.toLowerCase()) != -1;
+                    });
+                });
+        }
+
+        function submitReview() {
+            logger.info('Submitting');
+        }
+
+        function cancelEdit() {
+            $location.path('/reviews/' + vm.review.id);
+            logger.info('Editting cancelled');
+        }
     }
 })();
