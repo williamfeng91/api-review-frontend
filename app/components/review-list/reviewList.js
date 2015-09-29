d1d682f6fb29b9f0d36ac50f07ad620c56727e42
StanleyZSY <Stanleyzhang1019@gmail.com>
2015-09-29 17:25:17 +1000

    Updated review list

--- app/components/review-list/reviewList.js
+++ app/components/review-list/reviewList.js
@@ -5,10 +5,23 @@
         .module('app.reviewList')
         .controller('ReviewListController', ReviewListController);
 
-    function ReviewListController($routeParams, dataservice) {
+    function ReviewListController($routeParams, reviewservice,apiservice,userservice) {
         var vm = this;
-        dataservice.getReviews(0, 20).then(function(data) {
-            vm.reviews = data.items;
+		var count = 0;
+		var tatal = "";
+        reviewservice.getPage().then(function(reviews) {
+            vm.reviews = reviews.items;
+			count=count+1;
+			return createAPI(reviews.items[1].api,count,tatal);
         });
     }
+
+	function createAPI(aipId,count){
+	
+		var apiJsonId = aipId;
+		var apiJsonName = "new Api"+count;
+		count=count+1;
+		tatal += apiJsonId +" "+apiJsonName;
+	
+	}
 })();
