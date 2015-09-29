c87837edf319cfa4c1c5f183ff91b11246f4647f
Ian Mignaco <Ian.Mignaco@iress.com.au>
2015-09-30 00:24:05 +0100

    Revert "Merge pull request #44 from StanleyZSY/master"
    This reverts commit 7a6ea4af9c962e5b27d82b7eca008ed04c939646, reversing
    changes made to 8329b002b688c3136e880d5d7d45f188d1fdf6cf.

--- app/components/review-list/review-list.js
+++ app/components/review-list/review-list.js
@@ -1,64 +1,12 @@
 (function() {
     'use strict';
+
     angular
         .module('app.reviewList')
         .controller('ReviewListController', ReviewListController);
     function ReviewListController(reviewservice, logger) {
         var vm = this;
-		var offset = 2;
-		var limit = 100;
-		vm.PerPage = 20;
-		vm.count = 20;
-        reviewservice.getPage(1,100).then(function(data) {
-			vm.count = data.count;
-            vm.reviews = data.result;
-			vm.currentPage = 1;
-			
-			
-			//select a page
-			vm.selectPage = function(selected) {
-				if(vm.currentPage!=selected){
-	  			 	 vm.currentPage=selected;
-			  
-	  	         	 reviewservice.getPage(selected,100).then(function(data) {
-	  	           	  	vm.reviews = data.result;
-	  				});
-				}
-				
-			  
-			};
-			
-			//handler previous page
-			vm.prevPage = function() { 
-				if (vm.currentPage > 1) {
-					vm.currentPage--;
-	  	          reviewservice.getPage(vm.currentPage,100).then(function(data) {
-	  	            vm.reviews = data.result;
-	  				});
-				}
-			};
-			
-			//handler next page
-			vm.nextPage = function() {
-				if (vm.currentPage < vm.pageCount()) {
-					vm.currentPage++;
-	  	          reviewservice.getPage(vm.currentPage,100).then(function(data) {
-	  	            vm.reviews = data.result;
-	  				});
-				}
-			};
-			
-			//count page number
-			vm.pageCount = function() {
-				return Math.ceil(vm.count/vm.PerPage);
-			};
 
-			//get each page number
-			vm.pageRange = [];
-				for (var i=1; i<= vm.pageCount();i++) {
-					vm.pageRange.push(i);
-				}
-			
         vm.pageSize = 10;
         vm.doPaging = doPaging;
 
